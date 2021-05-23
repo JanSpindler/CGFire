49f5167d17715178040b9f3de665c83279ed72ce
Trampeltier <Vincent@VIncent-Schoenbach.de>
2021-05-23 19:42:03 +0200

    Projekterstellung

---
+++ include/framework/config.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+
+#define SHADER_ROOT std::string("E:/docs/Uni/SoSe 21/Computergraphik/Uebung/framework/shaders/")
+#define DATA_ROOT std::string("E:/docs/Uni/SoSe 21/Computergraphik/Uebung/framework/data/")
