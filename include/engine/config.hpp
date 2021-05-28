f37490fc7d27fe6413cc2b6f4de1c138f32538a1
Trampeltier <Vincent@VIncent-Schoenbach.de>
2021-05-28 13:03:47 +0200

    Stable Rucksack + Feuer läuft

--- include/engine/config.hpp
+++ include/engine/config.hpp
@@ -6,9 +6,16 @@
 #define CGFIRE_CONFIG_HPP
 
 #include <string>
+#include <filesystem>
 
-#define SHADER_ROOT std::string("../shaders")
-#define MODEL_ROOT std::string("../models")
-#define DATA_ROOT std::string("../data")
+//#define SHADER_ROOT std::string("../shaders")
+//#define MODEL_ROOT std::string("../models")
+//#define DATA_ROOT std::string("../data")
+
+#define ROOT_FOLDER std::filesystem::current_path().parent_path().parent_path().string()
+#define MODEL_ROOT (ROOT_FOLDER + std::string("/models/"))
+#define SHADER_ROOT (ROOT_FOLDER + std::string("/shaders/"))
+#define DATA_ROOT (ROOT_FOLDER + std::string("/data/"))
+//
 
 #endif //CGFIRE_CONFIG_HPP
--- include/framework/config.hpp
+++ include/framework/config.hpp
@@ -1,8 +1,11 @@
+//#pragma once
+//
+//#include <string>
+//#include <filesystem>
+//
+//#define ROOT_FOLDER std::filesystem::current_path().parent_path().parent_path().string()
+//#define SHADER_ROOT (ROOT_FOLDER + std::string("/shaders/"))
+//#define DATA_ROOT (ROOT_FOLDER + std::string("/data/"))
+//
 #pragma once
-
-#include <string>
-#include <filesystem>
-
-#define ROOT_FOLDER std::filesystem::current_path().parent_path().parent_path().string()
-#define SHADER_ROOT (ROOT_FOLDER + std::string("/shaders/"))
-#define DATA_ROOT (ROOT_FOLDER + std::string("/data/"))
+#include "engine/config.hpp"
